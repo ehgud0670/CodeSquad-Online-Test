6b74c59e974780ba80d4ba89cd0f2d6cedf473ad
ehgud0670 <kimdo2297@gmail.com>
2019-12-01 15:02:05 +0900

    Add : 야구 게임을 진행할 팀 생성

---
+++ src/kr/ac/ajou/main/Team.java
@@ -0,0 +1,48 @@
+package kr.ac.ajou.main;
+
+class Team {
+    private int hitsNum;
+    private int outNum;
+    private Hitter hitter;
+
+    Team() {
+        hitsNum = 0;
+        outNum = 0;
+        hitter = new Hitter();
+    }
+
+    int getHitsNum() {
+        return hitsNum;
+    }
+
+    int getOutNum() {
+        return outNum;
+    }
+
+    //about hitter
+    int getStrikeNum() {
+        return hitter.getStrikeNum();
+    }
+
+    int getBallNum() {
+        return hitter.getBallNum();
+    }
+
+    private static class Hitter {
+        private int strikeNum;
+        private int ballNum;
+
+        private Hitter() {
+            strikeNum = 0;
+            ballNum = 0;
+        }
+
+        int getStrikeNum() {
+            return strikeNum;
+        }
+
+        int getBallNum() {
+            return ballNum;
+        }
+    }
+}
