279567efecfff8e7557627f7cad1bb22d8327c7a
ehgud0670 <kimdo2297@gmail.com>
2019-12-03 02:36:07 +0900

    Add : 메뉴 출력 중 메뉴 옵션 추가

--- step-2/src/kr/ac/ajou/main/BaseballGame.java
+++ step-2/src/kr/ac/ajou/main/BaseballGame.java
@@ -1,8 +1,21 @@
 package kr.ac.ajou.main;
 
 class BaseballGame {
-    void turnOn(){
+    void turnOn() {
         printGameStart();
+        printMenu();
+    }
+
+    private void printMenu() {
+        printMenuOption();
+        
+    }
+
+    private void printMenuOption() {
+        System.out.println("1. 데이터 입력");
+        System.out.println("2. 데이터 출력");
+        System.out.println("3. 시합 시작");
+        System.out.println();
     }
 
     private void printGameStart() {
